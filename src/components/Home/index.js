356fcfe14aa7d1a90170c19985f14deb122d1e78
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2022-08-16 14:23:11 -0400

    fix lint issues

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,30 @@
 import { useState, useEffect } from 'react'
-import { Callout, Card } from '@telus-uds/ds-allium'
+import { Callout, Card, Icon, Table, StackView } from '@telus-uds/ds-allium'
 import { NextSeo } from 'next-seo'
 import seoConfig from './seo.config'
-import { Table, StackView } from '@telus-uds/ds-allium'
-import { Icon } from '@telus-uds/ds-allium'
-import { Document,  Clipboard, Delete } from '@telus-uds/palette-allium/build/rn/icons'
+import { Document, Clipboard, Delete } from '@telus-uds/palette-allium/build/rn/icons'
 import axios from 'axios'
 
 const PARTNERS_API_PATH = 'http://localhost:3000/api/partners'
 const locale = 'en-CA'
-const options = {year: 'numeric', month: 'short', day: 'numeric'}
+const options = { year: 'numeric', month: 'short', day: 'numeric' }
 
-const Home = () => { 
-  const [partners, setPartners] = useState([]);
+const Home = () => {
+  const [partners, setPartners] = useState([])
 
   useEffect(async () => {
     const fetchData = async () => {
       const result = await axios(
-        PARTNERS_API_PATH,
-      );
-      console.log(result.data.length);
+        PARTNERS_API_PATH
+      )
+      console.log(result.data.length)
       // console.log(result.data);
-      setPartners(result.data);
-      console.log(partners);
-    };
+      setPartners(result.data)
+      console.log(partners)
+    }
 
-    fetchData();
-  },[]);
+    fetchData()
+  },[])
   return (
   <>
     <NextSeo {...seoConfig} />
@@ -44,22 +42,7 @@ const Home = () => {
                 <Table.Cell></Table.Cell>
               </Table.Header>
               <Table.Body>
-                <Table.Row>
-                  <Table.Cell>1234</Table.Cell>
-                  <Table.Cell>Ivan Gabrovsky</Table.Cell>
-                  <Table.Cell>Ivan</Table.Cell>
-                  <Table.Cell>On-boarding with Settlement</Table.Cell>
-                  <Table.Cell>June 12 2022</Table.Cell>
-                  <Table.Cell>June 12 2027</Table.Cell>
-                  <Table.Cell>
-                    <StackView space={3} divider direction="row">
-                      <Icon icon={Document}  accessibilityLabel="More Info"/>
-                      <Icon icon={Clipboard} accessibilityLabel="Edit"/>
-                      <Icon icon={Delete} accessibilityLabel="Delete"/>
-                    </StackView>
-                  </Table.Cell>
-                </Table.Row>
-               {partners && partners.length > 0 && partners.map((partner) => {
+              {partners && partners.length > 0 && partners.map((partner) => {
                 return ( <Table.Row key={partner?.id}>
                   <Table.Cell>{partner?.id}</Table.Cell>
                   <Table.Cell>{partner?.name}</Table.Cell>
@@ -75,13 +58,14 @@ const Home = () => {
                     </StackView>
                   </Table.Cell>
                 </Table.Row>)
-               })}
+              })}
               </Table.Body>
             </Table>
         </Card>
       </Callout>
     </main>
   </>
-)}
+  )
+}
 
 export default Home
