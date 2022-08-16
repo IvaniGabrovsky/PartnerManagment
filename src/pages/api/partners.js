7ad6e9213e6b11b6b4eac187d7943b6ac372e178
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2022-08-16 11:38:04 -0400

    fix partners API

--- src/pages/api/partners.js
+++ src/pages/api/partners.js
@@ -1,11 +1,19 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+import path from 'path'
+import fs from 'fs'
+// import { promises as fs } from 'fs'
+
+export const filePathBuilder = () => path.join(process.cwd(), 'json', 'partners.json')
+
+export const extractData = (filePath) => {
+  const fileData = fs.readFileSync(filePath)
+  const data = JSON.parse(fileData)
+  return data
+}
 
 export default async function handler(req, res) {
-  //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'json');
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/partners.json', 'utf8');
-  //Return the content of the data file in json format
-  res.status(200).json(fileContents);
+  const filePath = filePathBuilder()
+  const data = extractData(filePath)
+  setTimeout(() => {
+    res.status(200).json(data)
+  }, 1000)
 }
