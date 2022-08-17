128f6a7315f6d87b055282c4d5c45648ee915810
Ivan Gabrovsky <x255748@corp.ads>
2022-08-17 14:27:58 -0400

    implemented flex grid with dummy data

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,15 +1,20 @@
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import {
-  Callout, Card, Typography, Spacer
+  Callout, Card, Typography, Spacer, Spinner
 } from '@telus-uds/ds-allium'
 import { NextSeo } from 'next-seo'
 import seoConfig from './seo.config'
+import { FlexGrid } from '@telus-uds/ds-allium'
+import { Box } from '@telus-uds/ds-allium'
+import {StackView} from '@telus-uds/ds-allium'
 // import axios from 'axios'
 
 // const PARTNER_API_PATH = 'http://localhost:3000/api/partner'
 
 const Info = () => {
+  const [partners, setPartners] = useState([])
+  const [loading, setLoading] = useState(false)
   const router = useRouter()
   const pathParameters = router?.query
   // console.log('########## pathParameters=', pathParameters, ' router=', router)
@@ -35,10 +40,109 @@ const Info = () => {
     <main>
         <Callout rounded>
           <Card>
-            <Typography variant={{ size: 'h1' }}>Partner Id: {id}</Typography>
-            <Spacer space={2} />
-            {/* <Spinner label="Fetching partners" show={loading}>
-            </Spinner> */}
+            {/* <Spinner label="Fetching partners" show={loading}> */}
+              <FlexGrid>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={4}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                      <Typography>ID: </Typography>
+                      <Typography>1234</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={8}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>Name: </Typography>
+                    </StackView>
+
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>Ivan Gabrovsky </Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                  <StackView>
+                    <Typography>Unique name:</Typography>
+                  </StackView>
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                  <StackView>
+                    <Typography>Ivan</Typography>
+                  </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>Partner status:</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>On-boarding with Settlement</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>Partner agreement start:</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>June 18, 2022</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+              <FlexGrid.Row>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>Partner agreement end:</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+                <FlexGrid.Col xs={3}>
+                  <Box variant={{ background: 'light' }} space={2}>
+                    <StackView>
+                    <Typography>June 18, 2026</Typography>
+                    </StackView>
+                  </Box>
+                </FlexGrid.Col>
+              </FlexGrid.Row>
+            </FlexGrid>
+
+            {/* </Spinner> */}
         </Card>
       </Callout>
     </main>
